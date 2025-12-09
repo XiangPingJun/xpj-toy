dd4f357e53b64d8511cf44a8d3b9ec7f3687aa21
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-12-09 15:56:09 +0800

    .

--- src/lib/components/media.svelte
+++ src/lib/components/media.svelte
@@ -11,69 +11,71 @@
   });
 </script>
 
-{#if mounted && (($imgUrl && $resources[$imgUrl]) || $videoUrl)}
-  <div
-    class={[
-      $mode === "Description"
-        ? "opacity-100 pointer-events-auto"
-        : "opacity-0 pointer-events-none",
-      "fixed left-1/2 -translate-x-1/2 top-3 transition-all",
-    ]}
-    transition:fly={{ y: 24 }}
-  >
+{#key `${$imgUrl}${$videoUrl}`}
+  {#if mounted && (($imgUrl && $resources[$imgUrl]) || $videoUrl)}
     <div
-      class="backdrop-blur-xs absolute top-[0.2rem] w-full h-[calc(100%-1rem)] -z-10"
-    ></div>
-    <div
-      class="rounded-tl-md rounded-tr-md border-3 border-b-0 border-white box-content bg-black/50 p-2 pb-0"
+      class={[
+        $mode === "Description"
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none",
+        "fixed left-1/2 -translate-x-1/2 top-3 transition-all",
+      ]}
+      transition:fly={{ y: 24 }}
     >
-      {#if $resources[$imgUrl]}
-        <img
-          src={$resources[$imgUrl]}
-          class={[
-            "max-w-[min(1360px,calc(100vw-2rem))] object-cover backdrop-blur-sm blur-bg",
-            $isPortrait
-              ? "max-h-[calc(100dvh-20rem)]"
-              : "max-h-[calc(100dvh-22rem)]",
-          ]}
-          alt=""
-        />
-      {:else if $resources[$videoUrl]}
-        <video
-          src={$resources[$videoUrl]}
-          class={[
-            "max-w-[min(1360px,calc(100vw-2rem))] object-cover backdrop-blur-sm blur-bg",
-            $isPortrait
-              ? "max-h-[calc(100dvh-20rem)]"
-              : "max-h-[calc(100dvh-22rem)]",
-          ]}
-          autoplay
-          muted
-          playsinline
-          loop
-          controls
-        >
-          <track kind="captions" />
-        </video>
-      {/if}
-    </div>
-    <div class="flex">
       <div
-        class="rounded-bl-md border-b-3 border-l-3 border-white box-content h-[1em] bg-black/50 flex-grow"
+        class="backdrop-blur-xs absolute top-[0.2rem] w-full h-[calc(100%-1rem)] -z-10"
       ></div>
-      <div class="bg-black/50 pt-1.25 px-0.5 font-bold flex items-center">
-        [<button
-          class="text-blue-400 hover:text-blue-300 cursor-pointer flex items-center"
-          onclick={() => {
-            $imgUrl = "";
-            $videoUrl = "";
-          }}
-          tabindex="-1"><CloseIcon />關閉1</button
-        >]
+      <div
+        class="rounded-tl-md rounded-tr-md border-3 border-b-0 border-white box-content bg-black/50 p-2 pb-0"
+      >
+        {#if $resources[$imgUrl]}
+          <img
+            src={$resources[$imgUrl]}
+            class={[
+              "max-w-[min(1360px,calc(100vw-2rem))] object-cover backdrop-blur-sm blur-bg",
+              $isPortrait
+                ? "max-h-[calc(100dvh-20rem)]"
+                : "max-h-[calc(100dvh-22rem)]",
+            ]}
+            alt=""
+          />
+        {:else if $resources[$videoUrl]}
+          <video
+            src={$resources[$videoUrl]}
+            class={[
+              "max-w-[min(1360px,calc(100vw-2rem))] object-cover backdrop-blur-sm blur-bg",
+              $isPortrait
+                ? "max-h-[calc(100dvh-20rem)]"
+                : "max-h-[calc(100dvh-22rem)]",
+            ]}
+            autoplay
+            muted
+            playsinline
+            loop
+            controls
+          >
+            <track kind="captions" />
+          </video>
+        {/if}
       </div>
-      <div
-        class="rounded-br-md border-b-3 border-r-3 border-white box-content h-[1em] bg-black/50 w-[2rem]"
-      ></div>
+      <div class="flex">
+        <div
+          class="rounded-bl-md border-b-3 border-l-3 border-white box-content h-[1em] bg-black/50 flex-grow"
+        ></div>
+        <div class="bg-black/50 pt-1.25 px-0.5 font-bold flex items-center">
+          [<button
+            class="text-blue-400 hover:text-blue-300 cursor-pointer flex items-center"
+            onclick={() => {
+              $imgUrl = "";
+              $videoUrl = "";
+            }}
+            tabindex="-1"><CloseIcon />關閉2</button
+          >]
+        </div>
+        <div
+          class="rounded-br-md border-b-3 border-r-3 border-white box-content h-[1em] bg-black/50 w-[2rem]"
+        ></div>
+      </div>
     </div>
-  </div>
-{/if}
+  {/if}
+{/key}
--- static/v/new-year-eve/data.js
+++ static/v/new-year-eve/data.js
@@ -23,8 +23,8 @@ export default {
       {
         text: '煙火結束後，人群散去，街道重新露出本來面貌，寒風掠過，帶著些微的清冷。',
         pov: '{"enabled":true,"minDistance":2.220446049250313e-16,"maxDistance":1.7976931348623157e+308,"minZoom":0.01,"maxZoom":1.7976931348623157e+308,"minPolarAngle":0,"maxPolarAngle":3.141592653589793,"minAzimuthAngle":-1.7976931348623157e+308,"maxAzimuthAngle":1.7976931348623157e+308,"smoothTime":0.04,"draggingSmoothTime":0.125,"dollySpeed":1,"truckSpeed":2,"dollyToCursor":false,"target":[0.0000032662384866169134,0.00021116427022806436,-0.00001347582156768358],"position":[-0.0005469099102072069,0.00013773849801895175,0.0005729246949292086],"zoom":0.35999999999999943,"focalOffset":[0,0,0],"target0":[0,0,0],"position0":[24.056042195026553,499.41928876495376,-1.2964734547150627],"zoom0":0.15000000000000002,"focalOffset0":[0,0,0]}',
-        videoUrl: "ants.mp4"
-        // videoUrl: "https://gunpla-hobby.github.io/NewYearEve/ants.mp4"
+        // videoUrl: "ants.mp4"
+        videoUrl: "https://gunpla-hobby.github.io/NewYearEve/ants.mp4"
       },
       {
         text: '我們在散場的人潮中漫步，你忽然問：「新年新希望是什麼？」\n我沉思了一下，答道：「我從不許新年新希望，因為它總是會被延到下一年，然後變成明年的新年新希望。再過幾年，可能還是一樣的答案。」',

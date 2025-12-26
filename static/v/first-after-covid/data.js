fc72fd1f184ab444cb9b03ae9d46c9c17b236225
XiangPingJun <XiangPingJun@users.noreply.github.com>
2025-12-26 18:23:15 +0800

    pg-cafe
    pg-cafe

--- static/v/first-after-covid/data.js
+++ static/v/first-after-covid/data.js
@@ -3,9 +3,9 @@ export default {
   title: '疫後初旅的全景瞬間',
   description: '疫情過後的日子，照片記錄了摘下口罩的瞬間自由',
   lines: [{
+    subjectUrl: "panorama.jpg",
     title: '疫後初旅的全景瞬間',
     pov: '{"enabled":true,"minDistance":2.220446049250313e-16,"maxDistance":1.7976931348623157e+308,"minZoom":0.01,"maxZoom":1.7976931348623157e+308,"minPolarAngle":0,"maxPolarAngle":3.141592653589793,"minAzimuthAngle":-1.7976931348623157e+308,"maxAzimuthAngle":1.7976931348623157e+308,"smoothTime":0.04,"draggingSmoothTime":0.125,"dollySpeed":1,"truckSpeed":2,"dollyToCursor":false,"verticalDragToForward":false,"target":[0,0,0],"position":[20.275404525928973,420.9307593356055,-1.0927202213218241],"zoom":0.16999999999999998,"focalOffset":[0,0,0],"target0":[0,0,0],"position0":[24.056042195026553,499.41928876495376,-1.2964734547150627],"zoom0":0.15000000000000002,"focalOffset0":[0,0,0]}',
-    subjectUrl: "panorama.jpg",
   }, {
     text: '我想你會覺得這是張沒有這麼特別的照片。\n不過對我來說卻是特別地有紀念價值，它是我在外拍攝的第一張360°全景相片。',
     pov: '{"enabled":true,"minDistance":2.220446049250313e-16,"maxDistance":1.7976931348623157e+308,"minZoom":0.01,"maxZoom":1.7976931348623157e+308,"minPolarAngle":0,"maxPolarAngle":3.141592653589793,"minAzimuthAngle":-1.7976931348623157e+308,"maxAzimuthAngle":1.7976931348623157e+308,"smoothTime":0.04,"draggingSmoothTime":0.125,"dollySpeed":1,"truckSpeed":2,"dollyToCursor":false,"verticalDragToForward":false,"target":[0,0,0],"position":[24.056042195025995,499.41928876495376,-1.2964734547150327],"zoom":0.15000000000000002,"focalOffset":[0,0,0],"target0":[0,0,0],"position0":[24.056042195026553,499.41928876495376,-1.2964734547150627],"zoom0":0.15000000000000002,"focalOffset0":[0,0,0]}',
